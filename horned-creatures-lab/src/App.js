9df7a25a62263da018d999ec5057289c3df8a0eb
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-02-10 19:37:02 -0800

    app.js

--- horned-creatures-lab/src/App.js
+++ horned-creatures-lab/src/App.js
@@ -6,11 +6,49 @@ import ImageList from './imageList/ImageList.js';
 
 
 export default class App extends React.Component {
+  state = {
+    keyword: '',
+    horns: 0,
+  }
+  handleKeywordChange = (e) => {
+    this.setState({
+      keyword: e.target.value
+    });
+  }
+  
+  
   render() {
+    const filteredImages = images.filter((image) => {
+      if (!this.state.horns) return true;
+      if (this.state.horns === image.horns) return true;
+      return false;})
+
     return (
       <>
         <Header />
-        <ImageList images={images} />
+        <form>
+          Creature Name
+          <input 
+            value={this.state.keyword}
+            onChange={this.handleKeywordChange}
+          />
+          Filter By:
+          <select
+           value={this.state.horns}
+           onChange={(e) => {
+             this.setState({
+               horns: e.target.value
+             })
+           }}
+          >
+            <option value='1'>1 Horn</option>
+            <option value='2'>2 Horns</option>
+            <option value='3'>3 Horns</option>
+            <option value='100'>100 Horns</option>
+          </select>
+          <button>Submit</button>
+        </form>
+        <ImageList filteredImages={filteredImages} />
       </>
     );
   }
--- horned-creatures-lab/src/imageList/ImageList.js
+++ horned-creatures-lab/src/imageList/ImageList.js
@@ -1,37 +1,19 @@
 import React from 'react';
 import ImageItem from '../imageItem.js';
-import images from '../data.js';
+
 
 export default class ImageList extends React.Component {
     render() {
-        state = {
-            keyword: '',
-            horns: 0,
-        }
 
-        // horn filter
-    const filteredImages = images.filter((image) => {
-        if (!this.state.horns) return true;
-        if (this.state.horns === image.horns) return true;
-        return false;
-      }).map(image => 
-        <ImageItem 
-          key={image.title}
-          image={image}/>);
+        const creatureImages = this.props.filteredImages.map((image) => {
+            <ImageItem
+                key={image.title}
+                ImageItemProp={image}
+            />})
         return (
-            <>
-                <form>
-                    Name
-                    <input />
-                    Filter By:
-                    <select>
-                        <option value='1'>1 Horn</option>
-                        <option value='2'>2 Horns</option>
-                        <option value='3'>3 Horns</option>
-                        <option value='100'>100 Horns</option>
-                    </select>
-                </form>
-            </>
+            <ul>
+               {creatureImages}
+            </ul>
         )
     }
 }
